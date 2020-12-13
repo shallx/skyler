2b3b132828130f25828890d2d4a716a2f1a43340
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2020-12-13 15:08:13 +0600

    Env cleaned, api organized, task organized, promise added for task command

--- src/api.js
+++ src/api.js
@@ -1,63 +1,60 @@
 const axios = require('axios');
+const path = require('path');
 
-require("dotenv").config();
-
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const key = process.env.TRELLO_KEY;
 const token = process.env.TRELLO_TOKEN;
 
+axios.defaults.baseURL = 'https://api.trello.com/1/';
+axios.defaults.params = {
+  key,
+  token
+}
+
 const createCard = async(name) => {
-  try {
-    const result = await axios.post('https://api.trello.com/1/cards', {
+  return new Promise(async(resolve, reject) => {
+    axios.post('cards', {
       name : name,
       idList : "5fc37294869f4a07606ae4b2",
-      key: key,
-      token : token
-    });
-    return result;
-  }
-  catch(err){
-    console.log(err);
-  }
+    })
+    .then(result => {
+      return resolve(result);
+    })
+    .catch(err =>{
+      reject("Some error occured");
+    })
+  })
 }
 
 const getCards = async() => {
-  try {
-    const result =await axios.get('https://api.trello.com/1/lists/5fc37294869f4a07606ae4b2/cards', {
-      params: {
-        key: "1a2d8146bad80cf8489c4f3fe1ccd62b",
-        token : "4ec2e42aae54caba0a651ac17d05c7d0ad875f0233ce0ee6b7070be4e92a892a"
-      }
-    });
-    return result.data.map((val,i) => {
-      return {
-        name : val.name,
-        id: val.id
-      }
-    });
-
-  }
-  catch(err){
-    console.log("Some error occured in the API");
-  }
+  return new Promise((resolve,reject) => {
+    axios.get('lists/5fc37294869f4a07606ae4b2/cards')
+      .then(result => {
+        const mapped_result = result.data.map((val,i) => {
+          return {
+            name : val.name,
+            id: val.id
+          }
+        });
+        resolve(mapped_result);
+      })
+      .catch(err => {
+        reject(err);
+      })
+  });
 }
 
 const removeCard = async(id) => {
-  try {
-    const result =await axios.delete('https://api.trello.com/1/cards/'+id, {
-      params: {
-        key: "1a2d8146bad80cf8489c4f3fe1ccd62b",
-        token : "4ec2e42aae54caba0a651ac17d05c7d0ad875f0233ce0ee6b7070be4e92a892a"
-      }
-    });
-    return "Success!!!";
-
-  }
-  catch(err){
-    console.log(err);
-  }
+  return new Promise((resolve, reject) => {
+    axios.delete('cards/'+id)
+      .then(result => {
+        resolve("Success");
+      })
+      .catch(err =>{
+        reject("Some error occured");
+      });
+  });
 }
-
-// createCard("Sleep late");
 
 module.exports = {
   createCard,
