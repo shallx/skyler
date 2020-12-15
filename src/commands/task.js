cf6b64dc088464cec070d92311c6d2f91db30cd8
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2020-12-15 14:56:32 +0600

    trello card description command added, task command rectified with async-await

--- src/commands/task.js
+++ src/commands/task.js
@@ -10,34 +10,26 @@ program
   .option("-r, --remove", "Remove task")
   .option("-l, --list", "List tasks")
   .option("-m, --move", "Move task to done")
+  .option("-d, --desc <desk>", "Description for created card")
   .description("Trello task management")
   .action(async(obj) => {
-    if(obj.add){
-      api.createCard(obj.add).then(result => {
-        chalk.blueBright("Task Added Successfully!!!")
+    try {
+      let result = null;
+      if(obj.add){
+        result = await api.createCard(obj.add);
+        if(obj.desc) result = await api.updateCard(result, obj.desc);
+        else result = "Task added Successfully!!!";
+      }
+      if(obj.list){
+        let list_id = await getListId();
+        let tasks = await api.getCards(list_id)
+        tasks.map((task, index) => console.log(index+1+ '. '+chalk.cyanBright(task.name)));
         process.exit();
-      }).catch(err => {
-        console.log(chalk.redBright(err));
-        process.exit();
-      });
-    }
-    if(obj.list){
-      api.getCards(obj.add)
-        .then(tasks => {
-          tasks.map((task, index) => console.log(index+1+ '. '+chalk.cyanBright(task.name)));
-          process.exit();
-        })
-        .catch(err => {
-          console.log(chalk.redBright(err));
-          process.exit();
-        })
-    }
-    if(obj.remove){
-      let cardS;
-      api.getCards('5fc37294869f4a07606ae4b3')
-        .then(cards => {
-          cardS = cards;
-          return inquirer
+      }
+      if(obj.remove){
+        let list_id = await getListId();
+        let cards = await api.getCards(list_id)
+        let answers = await inquirer
           .prompt([
             {
               type: 'list',
@@ -45,32 +37,18 @@ program
               message: 'Select a task to remove?',
               choices: cards.map((val) => val.name),
             },
-          ])
-        })
-        .then(async(answers) => {
-          let id;
-          cardS.forEach((card) => {
-            if(card.name == answers.task){
-              id = card.id;
-            }
-          });
-          return api.removeCard(id);
-        })
-        .then(result => {
-          chalk.blueBright(result)
-          process.exit();
-        })
-        .catch(err => {
-          console.log(chalk.red(err));
-          process.exit();
-        })
-    }
-    if(obj.move){
-      let cardS;
-      api.getCards()
-        .then(cards => {
-          cardS = cards;
-          return inquirer
+          ]);
+        let id;
+        cards.forEach((card) => {
+          if(card.name == answers.task){
+            id = card.id;
+          }
+        });
+        result = await api.removeCard(id);
+      }
+      if(obj.move){
+        let cards = await api.getCards()
+        let answers = await inquirer
           .prompt([
             {
               type: 'list',
@@ -78,24 +56,44 @@ program
               message: 'Select Completed Task',
               choices: cards.map((val) => val.name),
             },
-          ])
-        })
-        .then(async(answers) => {
-          let id;
-          cardS.forEach((card) => {
-            if(card.name == answers.task){
-              id = card.id;
-            }
-          });
-          return api.moveCard(id);
-        })
-        .then(result => {
-          console.log(chalk.blueBright(result));
-          process.exit();
-        })
-        .catch(err => {
-          console.log(chalk.red(err));
-          process.exit();
-        })
+          ]);
+        let id;
+        cards.forEach((card) => {
+          if(card.name == answers.task){
+            id = card.id;
+          }
+        });
+        result = await api.moveCard(id);
+      }
+      console.log(chalk.cyanBright(result));
+      
+      if(obj.desc && !obj.add) throw Error("--desc || -d should always be used with --add || -a");
+
+    } catch (error) {
+      console.log(chalk.redBright(error));
     }
+    process.exit();
   });
+
+async function getListId() {
+  let getList;
+  let list_id;
+  try {
+    getList = await inquirer.prompt(
+      [
+        {
+          type: 'list',
+          name: 'type',
+          message: 'Select a type of list',
+          choices: Object.keys(api.list)
+        }
+      ]
+    );
+    list_id = api.list[getList.type];
+    return list_id;
+
+  } catch (error) {
+    console.log(error);
+    process.exit();
+  }
+}
